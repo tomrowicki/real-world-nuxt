2260a812ab8e0b4b562d9883e79dd48c238c6640
Tomasz Mrowicki <tomrowicki@gmail.com>
2019-06-05 18:22:58 +0200

    Lesson 5 finished

---
+++ pages/event/create.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create your event here'
+        }
+      ]
+    }
+  }
+}
+</script>
